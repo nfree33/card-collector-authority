62db4a6b38d0184b1d14781445f5959416af7b0d
Neil Freeman <freeman.neil92@gmail.com>
2020-08-20 12:35:50 -0400

    test content

--- server.js
+++ server.js
@@ -21,7 +21,7 @@ const PORT = process.env.PORT || 3000;
 const MONGODB_URI = process.env.MONGODB_URI
 
 // Connect to Mongo
-mongoose.connect(MONGODB_URI ,  { useNewUrlParser: true});
+mongoose.connect(MONGODB_URI,  { useNewUrlParser: true});
 
 // Error / success
 db.on('error', (err) => console.log(err.message + ' is Mongod not running?'));
