f726d97dbddf0f133e896d11079726465f7d7ba6
Neil Freeman <freeman.neil92@gmail.com>
2020-08-26 12:12:22 -0400

    Oldschool reroutes to new

--- views/components/Default.jsx
+++ views/components/Default.jsx
@@ -23,7 +23,7 @@ class Default extends React.Component {
                                     <a className="nav-link" href="/">Home</a>
                                 </li>
                                 <li className="nav-item active">
-                                    <a className="nav-link" href="/oldschool">Kick it Old School <span className="sr-only">(current)</span></a>
+                                    <a className="nav-link" href="/oldschool/new">Kick it Old School <span className="sr-only">(current)</span></a>
                                 </li>
                                 <li className="nav-item">
                                     <a className="nav-link" href="/cards/new">Add Card</a>
